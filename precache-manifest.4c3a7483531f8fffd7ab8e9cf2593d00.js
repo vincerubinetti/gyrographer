4cf48aa54b69ea7d0fbff2c50303d9b400a8dc37
vincerubinetti <vince.rubinetti@gmail.com>
2019-12-30 03:30:21 +0000

    Deploying to gh-pages from  f68bb6c84c2b8fd8024163796a12feb5c4838ad4

---
+++ precache-manifest.4c3a7483531f8fffd7ab8e9cf2593d00.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7fb0d37df1062935d46b7b1592792c5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b1ea838bc57171327cfe",
+    "url": "./static/css/main.b562dd56.chunk.css"
+  },
+  {
+    "revision": "133b0abedf238bd0e5ba",
+    "url": "./static/js/2.3ed66c7b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.3ed66c7b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1ea838bc57171327cfe",
+    "url": "./static/js/main.1a84b7b1.chunk.js"
+  },
+  {
+    "revision": "94083229ef2bfc03c463",
+    "url": "./static/js/runtime-main.c08ce6d9.js"
+  },
+  {
+    "revision": "dcc629fdf7c1d0e96026df8b11bbd565",
+    "url": "./static/media/arrow_end.dcc629fd.svg"
+  },
+  {
+    "revision": "87bf51151dec11455ab79b3318d2e925",
+    "url": "./static/media/arrow_left.87bf5115.svg"
+  },
+  {
+    "revision": "17c0101ca511b6923494af1d5af2159f",
+    "url": "./static/media/arrow_right.17c0101c.svg"
+  },
+  {
+    "revision": "7ff23ac7541634002fa7a70b8f2fb1e0",
+    "url": "./static/media/arrow_start.7ff23ac7.svg"
+  },
+  {
+    "revision": "c79f4ea71b40444b20e220faeb4e8d69",
+    "url": "./static/media/axes.c79f4ea7.svg"
+  },
+  {
+    "revision": "1427d3e65dc75899dd852913d2eb7e88",
+    "url": "./static/media/bounds.1427d3e6.svg"
+  },
+  {
+    "revision": "bf43c72ca623bafbbd7f5005e36306bb",
+    "url": "./static/media/edit.bf43c72c.svg"
+  },
+  {
+    "revision": "ced9ad1c3e04d68426e21a75a55836ba",
+    "url": "./static/media/grid.ced9ad1c.svg"
+  },
+  {
+    "revision": "76d88770029b56c6265951835df50865",
+    "url": "./static/media/loop.76d88770.svg"
+  },
+  {
+    "revision": "db9e93bcf22b984dc3c06a145a455ae0",
+    "url": "./static/media/pause.db9e93bc.svg"
+  },
+  {
+    "revision": "c09b7c9ccf8a6eee93ae9588a4873f4c",
+    "url": "./static/media/play.c09b7c9c.svg"
+  },
+  {
+    "revision": "f59a64e65255ed27ae0d7f08504b67b1",
+    "url": "./static/media/redo.f59a64e6.svg"
+  },
+  {
+    "revision": "f347e4de6b987283452461e8d4a4e183",
+    "url": "./static/media/undo.f347e4de.svg"
+  }
+]);
